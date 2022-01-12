22922814efe848111e69595734504a4f26542fac
JD <jharalson29@gmail.com>
2022-01-12 18:06:53 -0500

    Updating the first test file

--- tests__/employee.test.js
+++ tests__/employee.test.js
@@ -1,11 +1,22 @@
-const Employee = require('..lib/Employee');
+const Employee = require('lib/Employee');
 const { expect } = require('@jest/globals');
 const { test } = require('picomatch');
-const employee = new Employee ('Jared', '456179', 'jharalson29@gmail.com');
+const employee = new Employee ('Jared', 456179, 'jharalson29@gmail.com');
 
-test('can we get the values', () => {
+test('can we get the object', () => {
     expect(employee.name).toBe('Jared')
-    expect(employee.id).toBe('456179')
+    expect(employee.id).toBe(456179)
     expect(employee.email).toBe('jharalson29@gmail.com')
+})
 
+test('can we get the employee name', () => {
+    expect(employee.getName()).toEqual(expect.any(String))
+})
+
+test('can we get employee id', () => {
+    expect(employee.getId()).toEqual(expect.any(Number))
+})
+
+test ('can we get the employee email', () => {
+    expect(employee.getEmail()).toEqual(expect.any(String))
 })
