c020566370901d359485ca11ffde8509c32033e0
JD <jharalson29@gmail.com>
2022-01-12 12:30:04 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// prompt for team member and their information
+// after prompts generate a team roster HTML that is formatted
+
+// when i click on an email address, it opens my default email messenger and plugs in the TO field with the email
+
+// when i click on github username, github is opened in a new tab
+
+// upon application start, prompt to enter team manager’s name, employee ID, email address, and office number
+
+
+// need to use inquirer to collect data
+// need to run jest to to run unit test
